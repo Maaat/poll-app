199052562f8fab5fbad7d221002602df84d2b034
Mat <matp@live.ca>
2016-11-03 02:55:57 -0400

    Fixed some paths that were broken after the project directory restructuring.

--- server/app.js
+++ server/app.js
@@ -53,13 +53,13 @@ passport.deserializeUser(function(id, cb) {
   });
 });
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// uncomment after placing your favicon in ../client/public-build
+//app.use(favicon(path.join(__dirname, '../client/public-build', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client/public-build')));
+app.use(express.static(path.join(__dirname, '../client/public-build')));
 app.use(expressSession({secret:'asdf', resave:false, saveUninitialized:false}));
 
 app.use(passport.initialize());
