1edb3b241e74e24654d5a3eb990b57c9b7157c08
Mathew Poff <matp@live.ca>
2016-11-14 12:54:26 -0500

    Connected Sequelize to Heroku's Postgres DB.

--- server/models/index.js
+++ server/models/index.js
@@ -8,10 +8,16 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
+if (env=='development') {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+else {
+  var sequelize = new Sequelize("postgres://dnubcsarqkeiuc:5u-g9cCehRg-b77XUXSt2IPTy5@ec2-50-17-220-39.compute-1.amazonaws.com:5432/d5ib88uq0u61pv", {
+    logging: false,
+    dialectOptions: {
+      ssl: true /* for SSL config since Heroku gives you this out of the box */
+    }
+  });
 }
 
 fs
